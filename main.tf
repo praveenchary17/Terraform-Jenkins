62e643ffa7c1261ae8ee23c7cfd65e15d535ad09
praveenchary <52570158+praveenchary17@users.noreply.github.com>
2023-05-15 11:54:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "foo" {
   ami           = "ami-016eb5d644c333ccb" # us-east-1
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   tags = {
       Name = "TF-Instance"
   }
